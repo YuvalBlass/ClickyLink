0fc61731ac058053d5072e8ef5a2a1a4055ac4e4
Yuval Blass <YuvalBlass@Yuvals-Mac-mini.local>
2019-07-10 11:55:20 +0300

    beta 1.0

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -6,20 +6,23 @@ from .auth import requires_auth
 
 page = Blueprint('page', __name__)
 
+
 @page.route('/<short_url>')
 def redirect_to_url(short_url):
     link = Link.query.filter_by(short_url=short_url).first_or_404()
     link.visits += 1
     db.session.commit()
-
+    return render_template('redirect.html',
+                           original_url=link.original_url)
     return redirect(link.original_url) 
+
 
 @page.route('/')
 @requires_auth
 def index():
     users = Link.query.all()
-    print(users)
     return render_template('index.html')
+
 
 @page.route('/remove_all')
 @requires_auth
@@ -28,8 +31,8 @@ def remove_all():
     for u in users:
         db.session.delete(u)
     db.session.commit()
-    print(users)
     return "Removed"
+
 
 @page.route('/add_link', methods=['POST'])
 @requires_auth
@@ -42,12 +45,14 @@ def add_link():
     return render_template('link_added.html', 
         new_link=link.short_url, original_url=link.original_url)
 
+
 @page.route('/info')
 @requires_auth
 def stats():
     links = Link.query.all()
 
     return render_template('info.html', links=links)
+
 
 @page.errorhandler(404)
 def page_not_found(e):
