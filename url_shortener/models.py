5d13bc3c93467974f0a744ab9549ba88506167b2
irishious <48771233+irymor@users.noreply.github.com>
2019-07-10 16:32:28 +0300

    Update models.py

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -26,9 +26,9 @@ class Link(db.Model):
         link = self.query.filter_by(short_url=short_url).first()
 
         if link:
-            return self.generate_short_link()
+            return self.generate_short_link(), requested == ""
         self.short_url = short_url
-        return short_url, False
+        return short_url, requested == ""
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
